3c601afd2d8c916e3149c116899130ba24fb1733
isKazumi <yudhistia110@gmail.com>
2023-08-01 22:48:47 +0800

    add desktop navbar

--- components/navigation/desktopnav/desktop-nav.jsx
+++ components/navigation/desktopnav/desktop-nav.jsx
@@ -3,15 +3,29 @@ import SearchItems from '../mobilenav/SearchItems'
 import DropdownMenu from '../dropdown-menu'
 import { BsThreeDotsVertical } from 'react-icons/bs'
 import { FaSearch } from 'react-icons/fa'
+import { IoIosArrowDown } from 'react-icons/io'
+import { useState } from 'react'
 
 const DesktopNav = () => {
+  const [isActive, setIsActive] = useState(true)
+
+  const hendlerClick = () => {
+    setIsActive(!isActive)
+  }
+
   return (
     <div className="w-full py-4">
       <div className="container mx-auto relative w-full flex items-center justify-between">
         <div className="flex items-center w-96 justify-between">
           <div className="text-2xl font-bold text-secondary">MEGA.news</div>
-          <div>
-            <button>Categories</button>
+          <div className="flex items-center flex-col gap-2 relative">
+            <button className="flex gap-2 items-center" onClick={hendlerClick}>
+              Categories
+              <span>
+                <IoIosArrowDown />
+              </span>
+            </button>
+            {isActive && <DropdownMenu active={isActive} />}
           </div>
         </div>
         <div className="w-96 relative top-0 left-0 overflow-hidden">
--- components/navigation/dropdown-menu.jsx
+++ components/navigation/dropdown-menu.jsx
@@ -1,7 +1,7 @@
 'use client'
 import Link from 'next/link'
 
-const DropdownMenu = () => {
+const DropdownMenu = ({ active }) => {
   const menu = {
     business: 'Busines',
     entertainment: 'Entertainment',
@@ -12,11 +12,17 @@ const DropdownMenu = () => {
   }
 
   return (
-    <div className="bg-gray transition duration-300 ease-in p-2">
+    <div
+      className={`transition duration-300 ease-in p-2 ${
+        active
+          ? 'bg-white shadow-lg absolute top-6 left-0 w-60 rounded-lg p-4 '
+          : 'bg-gray'
+      }`}
+    >
       <div className="flex flex-col gap-2 ">
         {Object.entries(menu).map(([key, value]) => (
           <Link
-            className="border-b border-b-slate-500 py-2"
+            className="border-b  border-b-slate-500 py-2 hover:text-secondary"
             key={key}
             href={`/posts/${key}`}
           >
